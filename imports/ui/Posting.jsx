10e4542f8c2587ea53f9f66e4b6346987ab60122
Danya Karras <Danya.Karras@D2L.com>
2019-01-26 14:57:12 -0800

    forgot to add posting component

---
+++ imports/ui/Posting.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Posting extends Component {
+
+  render() {
+    return (
+      <div>
+        <h4>This is a posting!</h4>
+      </div>
+    );
+  }
+}
