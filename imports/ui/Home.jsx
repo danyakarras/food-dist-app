fbaf90e2e64b9196b293bbf75bf818ac4abfeef9
Katarina <kat9234@gmail.com>
2019-01-26 17:35:43 -0800

    Some design for postings

--- imports/ui/Home.jsx
+++ imports/ui/Home.jsx
@@ -9,9 +9,9 @@ export default class Home extends Component {
         <h2>This component will be the base of our homepage!</h2>
         <p>More text blah blah blah</p>
         <p>More profile page</p>
-        <Posting />
-        <Posting />
-        <Posting />
+        <Posting name="Gala Apples" restaurant="Walmart" time="until 5pm" price="$0.50 /lb" quantity="4" address="12 blank street"/>
+        <Posting name="Gala Apples" restaurant="Walmart" time="until 5pm" price="$0.50 /lb" quantity="3" address="12 blank street"/>
+        <Posting name="Gala Apples" restaurant="Walmart" time="until 5pm" price="$0.50 /lb" quantity="2" address="12 blank street"/>
       </div>
     );
   }
