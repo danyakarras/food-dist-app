0ddf6e86b040a1791b19d923e0d248ba4c5ddf64
Amy Hong <amyhong0502@gmail.com>
2019-01-26 17:22:46 -0800

    added schema for restaurant

--- imports/api/restaurant.js
+++ imports/api/restaurant.js
@@ -1,3 +1,13 @@
 import { Mongo } from 'meteor/mongo';
+import AddressSchema from './address';
 
 export default Restaurant = new Mongo.Collection('restaurant');
+
+Restaurant.schema = new SimpleSchema({
+  name: { type: String },
+  address: {
+    type: AddressSchema
+  },
+  phone: { type: String },
+  email: { type: String }
+});
