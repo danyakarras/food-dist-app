7ad0a31d96bf7f388509a8c2a831305536997e64
Danya Karras <Danya.Karras@D2L.com>
2019-01-26 21:29:50 -0800

    removed file no longer needed

--- client/main.jsx
+++ client/main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import { renderRoutes } from '../imports/routes.js';
-import App from '/imports/ui/App';
 
 Meteor.startup(() => {
   render(renderRoutes(), document.getElementById('react-target'));
