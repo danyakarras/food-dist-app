fb66b50d2a1525ad11eae08434b9e843f99b5080
Amy Hong <amyhong0502@gmail.com>
2019-01-26 22:33:50 -0800

    removed template schema

--- imports/api/deal.js
+++ imports/api/deal.js
@@ -1,11 +1,12 @@
 import { Mongo } from 'meteor/mongo';
 
-export default (Deal = new Mongo.Collection('deal'));
+export default Deal = new Mongo.Collection('deal');
 
 Deal.schema = new SimpleSchema({
   name: { type: String },
-  menuId: { type: String },
-  discount: { type: Number },
+  restaurantId: { type: String },
+  price: { type: Number },
+  priceUnit: { type: String },
   validUntil: { type: Date },
   quantity: { type: String, optional: true }
 });
