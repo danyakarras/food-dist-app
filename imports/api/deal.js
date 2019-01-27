af12a644753997e737b3232c41d87a78f364fc54
Amy Hong <amyhong0502@gmail.com>
2019-01-26 18:07:20 -0800

    added quantity field on deal schema

--- imports/api/deal.js
+++ imports/api/deal.js
@@ -5,5 +5,6 @@ export default Deal = new Mongo.Collection('deal');
 Deal.schema = new SimpleSchema({
   name: { type: String },
   menuId: { type: String },
-  validUntil: { type: Date }
+  validUntil: { type: Date },
+  quantity: { type: Number, optional: true }
 });
