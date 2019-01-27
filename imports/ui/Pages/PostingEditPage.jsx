076873d2b2a660dba969a993eb96e5fb1e1d7481
Danya Karras <Danya.Karras@D2L.com>
2019-01-27 09:20:55 -0800

    back to button

--- imports/ui/Pages/PostingEditPage.jsx
+++ imports/ui/Pages/PostingEditPage.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Deal from '/imports/api/deal';
 import NavBar from '../SmallComponents/NavBar.jsx';
+import { NavLink } from 'react-router-dom';
 
 export default class PostingEditPage extends Component {
   state = {
@@ -44,58 +45,60 @@ export default class PostingEditPage extends Component {
       <div>
         <NavBar />
         <div className='page-width'>
-          <h1>Hello World</h1>
+	        <div id="back-to-bar">
+	        	<NavLink to="/providerProfile" id="back-to-text"><img id="back-arrow" src="/green-large.png"/>Back to my Table</NavLink>
+	        </div>
           <div id='posting-image'>
             <img src='/gala-apples.jpg' />
           </div>
           <form id='posting-create-form' onSubmit={this.onSubmit}>
-            <label>Name</label>
+            <div>Name</div>
             <input
               id='name'
               name='name'
               type='text'
-              className='baloo-font'
+              className='edit-form baloo-font'
               placeholder='Apples'
               value={this.state.name}
               onChange={this.onInputChange}
             />
-            <label>Price</label>
+            <div>Price</div>
             $
             <input
               id='price'
               name='price'
               type='text'
-              className='baloo-font'
+              className='edit-form baloo-font'
               placeholder='00.50'
               value={this.state.price}
               onChange={this.onInputChange}
             />
-            <label>Unit</label>
+            <div>Unit</div>
             <input
               id='unit'
               name='unit'
               type='text'
-              className='baloo-font'
+              className='edit-form baloo-font'
               placeholder='lb'
               value={this.state.unit}
               onChange={this.onInputChange}
             />
-            <label>Quantity (optional)</label>
+            <div>Quantity (optional)</div>
             <input
               id='quantity'
               name='quantity'
               type='text'
-              className='baloo-font'
+              className='edit-form baloo-font'
               placeholder='20lbs'
               value={this.state.quantity}
               onChange={this.onInputChange}
             />
-            <label>Duration</label>
+            <div>Duration</div>
             <input
               id='startTime'
               name='startTime'
               type='text'
-              className='baloo-font'
+              className='edit-form baloo-font'
               placeholder='3:30PM'
               value={this.state.startTime}
               onChange={this.onInputChange}
@@ -104,12 +107,12 @@ export default class PostingEditPage extends Component {
               id='endTime'
               name='endTime'
               type='text'
-              className='baloo-font'
+              className='edit-form baloo-font'
               placeholder='6:30PM'
               value={this.state.endTime}
               onChange={this.onInputChange}
             />
-            <input type='submit' value='SUBMIT' />
+            <input id="submit-button" type='submit' value='SUBMIT' />
           </form>
         </div>
       </div>
